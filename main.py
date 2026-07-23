88ea79417bd897248d496f150d1a161b3f92d12d
Bassant Arafa <Bassanteharafa@gmail.com>
2026-07-23 22:03:08 +0300

    Updated main.py to load DICOM files for patient1.1 on 3Dircad

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from src.medical_volume import MedicalVolume
-temporary_ct_data = "3D CT scan data"
-volume = MedicalVolume(voxel_data=temporary_ct_data, spacing=(1.0, 1.0, 1.0), origin=(0.0, 0.0, 0.0), direction=(1.0, 0.0, 0.0), metadata={"patient_id": "12345", "scan_date": "2023-01-01"})
-print(volume.voxel_data)  # Output: 3D CT scan data
-
-
-
+from src.Image_Loader import load_dicom
+folder_path =r"C:\Users\Bassant\Downloads\Liver Digital Twin Project\User Experience\3Dircadb files Cases\3Dircadb1.1\3Dircadb1.1\PATIENT_DICOM\PATIENT_DICOM"
+print("Loading DICOM files from folder:", folder_path)
+volume = load_dicom(folder_path)
+#simple data extraction 
+print("Voxel data shape:", volume.voxel_data.shape)
+print("Spacing:", volume.spacing)
+print("Origin:", volume.origin)
