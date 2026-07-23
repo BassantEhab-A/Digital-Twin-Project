3ffc05ff68d1efe1fe6362113c6da4ce06e17975
Bassant Arafa <Bassanteharafa@gmail.com>
2026-07-23 21:35:56 +0300

    Initial commit

--- src/medical_volume.py
+++ src/medical_volume.py
@@ -1,16 +1,16 @@
 class MedicalVolume:         # telling python a new datatype called Medical volume
-    def __init__(self, voxel_data, spacing, origin, direction, metadata=None):
+    def __init__(self, voxel_data, spacing, origin, direction, metadata=None):         #metadata = None, so that we can store any additional information about the volume, such as patient information, scan parameters, etc. 
+
         self.voxel_data = voxel_data
         self.spacing = spacing
         self.origin = origin
         self.direction = direction
         self.metadata = metadata
         self.segmentations={}   # Empty dictionaries mean: # we reserve places for fututre results
-        self.features={}
+        self.features={}  #Empty dictionaries mean:"We don't have results yet, but modules can add them later."
 
-        # we reserve places for fututre results
-            #Empty dictionaries mean:"We don't have results yet, but modules can add them later."
-        #Now our object knows: the acutal ct values (voxel_data), the physical size(spacing), the location(origin), the orientation(direction)
-        #next step : add metadata = None, so that we can store any additional information about the volume, such as patient information, scan parameters, etc. This will make our MedicalVolume class more versatile and useful in different contexts.
+#Now the object knows: the acutal ct values (voxel_data), the physical size(spacing), the location(origin), the orientation(direction)
+
+            
 
  
